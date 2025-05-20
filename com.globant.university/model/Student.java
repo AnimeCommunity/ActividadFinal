69f1771f4eb8855fecee7799f93f8351a785d012
aniokku <>
2025-05-20 14:38:47 -0500

    student creado 2.0

--- com.globant.university/model/Student.java
+++ com.globant.university/model/Student.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Student {
+    private static int nextId = 1;
+    private int id;
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.id = nextId++;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | " + name + " | Age: " + age;
+    }
     
 }
